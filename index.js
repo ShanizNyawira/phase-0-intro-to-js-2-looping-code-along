e6db12e50598deedde5d4dc8fca33aaf01301852
Shaniz Nyawira <roselynshaniz@gmail.com>
2022-08-26 12:56:40 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const openPalm = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(openPalm , string) {
+   
+       const newopenPalm = [];
+    for (let i = 0; i < openPalm.length; i++)
+{newopenPalm.push(`Thank you, ${openPalm[i]}, for the wonderful ${string} gift!`)}
+ return newopenPalm;
+}
+
+console.log(writeCards(openPalm,'surprise'));
+function countDown(number) {
+    while (number >=0){
+        console.log(number--);
+    }
+}
